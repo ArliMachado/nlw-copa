a4678c27278458810437ed4c51f17dd02e52355a
Arli Machado <arli.machado@gmail.com>
2022-11-21 23:23:39 -0300

    definindo serviço de contagem de pools

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,28 @@
+import { PrismaClient } from '@prisma/client'
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
+
+const prisma = new PrismaClient({
+  log: ['query']
+})
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true
   })
 
-  fastify.get('/pools/count', () => {
-    return { count: 0}
+  await fastify.register(cors, {
+    origin: true
+  })
+
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count()
+    return {count}
   })
 
   await fastify.listen({
-    port: 3333
+    port: 3333,
+    host: '0.0.0.0'
   })
 }
 
